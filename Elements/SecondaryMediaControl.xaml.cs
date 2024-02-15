9312cc427209aece74e7a15fbc4498b7ed8a43b0
DaNussi <daniel.nussbaum@htlstp.at>
2024-02-15 23:45:35 +0100

    Version 2

---
+++ Elements/SecondaryMediaControl.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace Vidiya.Elements
+{
+    /// <summary>
+    /// Interaktionslogik für SecondaryMediaControl.xaml
+    /// </summary>
+    public partial class SecondaryMediaControl : UserControl
+    {
+        public SecondaryMediaControl()
+        {
+            InitializeComponent();
+        }
+
+        bool IsMuted = false;
+        private void VolumeButton_Click(object sender, RoutedEventArgs e)
+        {
+            IsMuted = !IsMuted;
+            if(IsMuted)
+                VolumeIcon.Kind = Material.Icons.MaterialIconKind.VolumeMute;
+            else 
+                VolumeIcon.Kind = Material.Icons.MaterialIconKind.VolumeHigh;
+        }
+    }
+}
---
+++ Elements/SidePanel.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace Vidiya.Elements
+{
+    /// <summary>
+    /// Interaktionslogik für SidePanel.xaml
+    /// </summary>
+    public partial class SidePanel : UserControl
+    {
+        public SidePanel()
+        {
+            InitializeComponent();
+        }
+    }
+}
