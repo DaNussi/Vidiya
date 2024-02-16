34a1462e827a8c3ceef20772a4fbb498e96064f1
DaNussi <daniel.nussbaum@htlstp.at>
2024-02-16 23:55:32 +0100

    Vlume Control

--- Elements/SecondaryMediaControl.xaml.cs
+++ Elements/SecondaryMediaControl.xaml.cs
@@ -16,11 +16,46 @@ namespace Vidiya.Elements
         bool IsMuted = false;
         private void VolumeButton_Click(object sender, RoutedEventArgs e)
         {
-            IsMuted = !IsMuted;
             if(IsMuted)
-                VolumeIcon.Kind = Material.Icons.MaterialIconKind.VolumeMute;
+                UnMute();
             else 
+                Mute();
+        }
+
+        private void VolumeSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            SetVolume(e.NewValue);
+        }
+
+        public void Mute ()
+        {
+            if (IsMuted) return;
+            IsMuted = true;
+            VolumeIcon.Kind = Material.Icons.MaterialIconKind.VolumeMute;
+        }
+
+        public void UnMute()
+        {
+            if (!IsMuted) return;
+            IsMuted = false;
+
+            if(VolumeIcon.Kind == Material.Icons.MaterialIconKind.VolumeMute) SetVolume(VolumeSlider.Value);
+        }
+
+        public void SetVolume (double value)
+        {
+
+            if(value == 0)
+            {
+                Mute();
+                return;
+            } 
+
+            UnMute();
+            if (value > 0.5)
                 VolumeIcon.Kind = Material.Icons.MaterialIconKind.VolumeHigh;
+            else
+                VolumeIcon.Kind = Material.Icons.MaterialIconKind.VolumeMedium;
         }
     }
 }
